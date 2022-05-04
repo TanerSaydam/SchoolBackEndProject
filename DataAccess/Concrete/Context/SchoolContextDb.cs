5146dcd596cdfcd6247404050946c53f5f9955bf
Taner Saydam <tanersaydam@gmail.com>
2022-05-04 23:19:05 +0300

    Add project files.

---
+++ DataAccess/Concrete/Context/SchoolContextDb.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.Context
+{
+    public class SchoolContextDb : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-3BJ5GK9;Database=SchoolDb;Integrated Security=false");
+        }        
+    } 
+}
---
+++ WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        SqlConnection con = new SqlConnection("server=DESKTOP-3BJ5GK9;Database=SchoolDb;Integrated Security=false");
+
+        [HttpGet("get")]
+        public IActionResult Values()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("Select * From Department",con);
+            DataTable da = new DataTable();
+            da.Clear();
+            adapter.Fill(da);
+
+            return Ok(da);
+        }
+    }
+}
