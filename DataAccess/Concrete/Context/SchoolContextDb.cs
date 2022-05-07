befc25a6202ffcd326a762474014894a4c1da6ad
Taner Saydam <tanersaydam@gmail.com>
2022-05-07 05:39:10 +0300

    .

--- DataAccess/Concrete/Context/SchoolContextDb.cs
+++ DataAccess/Concrete/Context/SchoolContextDb.cs
@@ -12,7 +12,18 @@ namespace DataAccess.Concrete.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("server=DESKTOP-3BJ5GK9;Database=SchoolDb;Integrated Security=false");
-        }        
-    } 
+            optionsBuilder.UseSqlServer("server=DESKTOP-3BJ5GK9;Database=SchoolDb;Integrated Security=true");
+        }
+
+        public virtual DbSet<Adminstrator> Adminstrators { get; set; }
+        public virtual DbSet<Class> Classes { get; set; }
+        public virtual DbSet<ClassStudent> ClassStudents { get; set; }
+        public virtual DbSet<ClassroomLesson> ClassroomLessons { get; set; }
+        public virtual DbSet<ClassroomTeacher> ClassroomTeachers { get; set; }
+        public virtual DbSet<Lesson> Lessons { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<StudentStatus> StudentStatuses { get; set; }
+        public virtual DbSet<Teacher> Teachers { get; set; }
+
+    }   
 }
